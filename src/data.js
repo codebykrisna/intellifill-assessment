d295e9d3d1120fb0c54ab2c52e949cba2ba3c6b0
codebykrisna <112065383+codebykrisna@users.noreply.github.com>
2024-01-14 21:31:00 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,65 @@
+const Examdata = [
+    {
+        id:1,
+        image:"https://static.toiimg.com/thumb/msid-94213168,imgsize-13574,width-400,resizemode-4/94213168.jpg",
+        title:"JEE Mains 2023",
+        subtitle:"National Testing Agency",
+        member:"More than 20 Lakhs",
+        price:"₹ 1200",
+        tag1:"Engineering",
+        tag2:"PCM",
+    },
+    {
+        id:2,
+        image:"https://cdn.dribbble.com/users/2843822/screenshots/6568646/upsc_logo_submission.png",
+        title:"Civil Services 2023",
+        subtitle:"Union Public Service Commission",
+        member:"More than 15 Lakhs",
+        price:"₹ 1500",
+        tag1:"Engineering",
+        tag2:"PCM",
+
+    },
+    {
+        id:3,
+        image:"https://w7.pngwing.com/pngs/381/126/png-transparent-scc-staff-selection-commission-logo.png",
+        title:"SSC CGL 2023",
+        subtitle:"Staff Selection Commission",
+        member:"More than 10 Lakhs",
+        price:"₹ 1000",
+        tag1:"Engineering",
+        tag2:"PCM",
+    },{
+        id:4,
+        image:"https://images.collegedunia.com/public/college_data/images/logos/1416489794logo.png?h=55&w=55&mode=stretch",
+        title:"VITEEE 2023",
+        subtitle:"Vellore institute of Technology",
+        member:"More than 20 Lakhs",
+        price:"₹ 1500",
+        tag1:"Engineering",
+        tag2:"PCM",
+    },
+    {
+        id:5,
+        image:"https://static.kabaddiadda.com/images/playerImages/aiiu/7310_SRM_University.png",
+        title:"SRMJEE 2023",
+        subtitle:"SRMIST",
+        member:"More than 17 Lakhs",
+        price:"₹ 1200",
+        tag1:"Engineering",
+        tag2:"PCM",
+
+    },
+    {
+        id:6,
+        image:"http://www.classresult.in/_images/articles/images/2021-09/8283_2_94318.jpg",
+        title:"TS EAMCT 2023",
+        subtitle:"TSCHE",
+        member:"More than 10 Lakhs",
+        price:"₹ 1000",
+        tag1:"Engineering",
+        tag2:"PCM",
+    }
+];
+
+export default Examdata;
